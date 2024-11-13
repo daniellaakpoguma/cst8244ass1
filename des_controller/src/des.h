8a1fc53053f7d375143070f65357fc9883338be1
Daniella Akpoguma <rukevweakpoguma11@gmail.com>
2024-11-13 11:43:56 -0500

    Made some changes

--- des_controller/src/des.h
+++ des_controller/src/des.h
@@ -8,40 +8,69 @@
 #ifndef DES_H_
 #define DES_H_
 
-#define NUM_STATES 14 // Number of states in FSM
+#define NUM_STATES 11 // Number of states in FSM
+
+//typedef enum {
+//    INIT_STATE = 0,                // Initializing state
+//    LEFT_DOOR_SCAN_STATE = 1,      // Left door scan state
+//    GUARD_LEFT_UNLOCK_STATE = 2,   // Guard left unlock state
+//    LEFT_DOOR_OPEN_STATE = 3,      // Left door open state
+//    WEIGHT_CHECK_STATE = 4,        // Weight check state
+//    LEFT_DOOR_CLOSE_STATE = 5,     // Left door close state
+//    GUARD_LEFT_LOCK_STATE = 6,     // Guard left lock state
+//    GUARD_RIGHT_UNLOCK_STATE = 7,  // Guard right unlock state
+//    RIGHT_DOOR_OPEN_STATE = 8,     // Right door open state
+//    RIGHT_DOOR_CLOSE_STATE = 9,    // Right door close state
+//    GUARD_RIGHT_LOCK_STATE = 10,   // Guard right lock state
+//    RIGHT_DOOR_SCAN_STATE = 11,    // Right door scan state
+//    LOCK_DOWN_STATE = 12,          // Lock-down state
+//    SYSTEM_EXIT_STATE = 13,        // Exit state
+//} State;
 
 typedef enum {
-    INIT_STATE = 0,                // Initializing state
-    LEFT_DOOR_SCAN_STATE = 1,      // Left door scan state
-    GUARD_LEFT_UNLOCK_STATE = 2,   // Guard left unlock state
-    LEFT_DOOR_OPEN_STATE = 3,      // Left door open state
-    WEIGHT_CHECK_STATE = 4,        // Weight check state
-    LEFT_DOOR_CLOSE_STATE = 5,     // Left door close state
-    GUARD_LEFT_LOCK_STATE = 6,     // Guard left lock state
-    GUARD_RIGHT_UNLOCK_STATE = 7,  // Guard right unlock state
-    RIGHT_DOOR_OPEN_STATE = 8,     // Right door open state
-    RIGHT_DOOR_CLOSE_STATE = 9,    // Right door close state
-    GUARD_RIGHT_LOCK_STATE = 10,   // Guard right lock state
-    RIGHT_DOOR_SCAN_STATE = 11,    // Right door scan state
-    LOCK_DOWN_STATE = 12,          // Lock-down state
-    SYSTEM_EXIT_STATE = 13,        // Exit state
+    INIT_STATE = 0,           // Initializing state
+	WAIT_SCAN_STATE = 1,	  // Person id waiting to be scannerd
+    DOOR_SCAN_STATE = 3,      // Person id scanned door state
+    DOOR_UNLOCKED_STATE = 4,  // Guard unlock door state
+    DOOR_OPEN_STATE = 5,      // Door open state
+    WAIT_WEIGHT_STATE = 6,    // Person weight wiating to be scanned
+	WEIGHT_CHECK_STATE = 7,   // Weight check state
+    DOOR_CLOSE_STATE = 8,     // Door closed state
+    DOOR_LOCKED_STATE = 9,    // Guard lock door state
+	SYSTEM_EXIT_STATE = 10,   // Exit state
 } State;
+
+
+// Define an enumeration for the event types
+//typedef enum {
+//    ls,     // Left scan event
+//    rs,     // Right scan event
+//    ws,     // Weight scale event
+//    lo,     // Left open event
+//    ro,     // Right open event
+//    lc,     // Left closed event
+//    rc,     // Right closed event
+//    gru,    // Guard unlock right event
+//    grl,    // Guard lock right event
+//    glu,    // Guard unlock left event
+//    gll,     // Guard lock left event
+//} EventType;
 
 #define NUM_INPUTS 13 // Number of input commands
 typedef enum {
-    LEFT_SCAN_EVT = 0,        // Left scan event
+    LEFT_SCAN_EVT = 0,         // Left scan event
     GUARD_LEFT_UNLOCK_EVT = 1, // Guard left unlock event
-    LEFT_DOOR_OPEN_EVT = 2,   // Left door open event
-    WEIGHT_CHECK_EVT = 3,     // Weight check event
-    LEFT_DOOR_CLOSE_EVT = 4,  // Left door close event
-    GUARD_LEFT_LOCK_EVT = 5,  // Guard left lock event
+    LEFT_DOOR_OPEN_EVT = 2,    // Left door open event
+    WEIGHT_CHECK_EVT = 3,      // Weight check event
+    LEFT_DOOR_CLOSE_EVT = 4,   // Left door close event
+    GUARD_LEFT_LOCK_EVT = 5,   // Guard left lock event
     GUARD_RIGHT_UNLOCK_EVT = 6, // Guard right unlock event
-    RIGHT_DOOR_OPEN_EVT = 7,  // Right door open event
-    RIGHT_DOOR_CLOSE_EVT = 8, // Right door close event
-    GUARD_RIGHT_LOCK_EVT = 9, // Guard right lock event
-    RIGHT_SCAN_EVT = 10,      // Right scan event
-    EXIT_EVT = 11,            // Exit event
-    LOCK_DOWN_EVT = 12,       // Lock-down event
+    RIGHT_DOOR_OPEN_EVT = 7,   // Right door open event
+    RIGHT_DOOR_CLOSE_EVT = 8,  // Right door close event
+    GUARD_RIGHT_LOCK_EVT = 9,  // Guard right lock event
+    RIGHT_SCAN_EVT = 10,       // Right scan event
+    EXIT_EVT = 11,             // Exit event
+    LOCK_DOWN_EVT = 12,        // Lock-down event
 } Input;
 
 const char *inMessage[NUM_INPUTS] = {
@@ -98,7 +127,7 @@ typedef struct {
     int person_id;     // Person's unique ID
     int weight;        // Person's weight
     int state;         // Person's current state
-    int direction;     // Direction: inbound or outbound (e.g., 0 = inbound, 1 = outbound)
+    int direction;     // Direction: inbound or outbound (e.g., 0 = inbound, 1 = outbound) // we may not use this
 } Person;
 
 // Display struct for storing output messages and the associated person
--- des_inputs/src/des_inputs.c
+++ des_inputs/src/des_inputs.c
@@ -37,73 +37,74 @@ void displayEventChoices() {
     printf("\t%s = Exit\n", EXIT_EVT);
 }
 
-// Function to map user input to an event code
-int getEventCode(const char *event) {
-    if (strncmp(event, LEFT_SCAN_EVT, strlen(LEFT_SCAN_EVT)) == 0) return 0;
-    if (strcmp(event, GUARD_LEFT_UNLOCK_EVT), strlen(GUARD_LEFT_UNLOCK_EVT) == 0) return 1;
-    if (strcmp(event, LEFT_OPEN_EVT), strlen(LEFT_OPEN_EVT) == 0) return 2;
-    if (strcmp(event, WEIGHT_EVT), strlen(WEIGHT_EVT) == 0) return 3;
-    if (strcmp(event, LEFT_CLOSE_EVT), strlen(LEFT_CLOSE_EVT) == 0) return 4;
-    if (strcmp(event, GUARD_LEFT_LOCK_EVT), strlen(GUARD_LEFT_LOCK_EVT) == 0) return 5;
-    if (strcmp(event, GUARD_RIGHT_UNLOCK_EVT), strlen(GUARD_RIGHT_UNLOCK_EVT) == 0) return 6;
-    if (strcmp(event, RIGHT_OPEN_EVT), strlen(RIGHT_OPEN_EVT) == 0) return 7;
-    if (strcmp(event, RIGHT_CLOSE_EVT), strlen(RIGHT_CLOSE_EVT) == 0) return 8;
-    if (strcmp(event, GUARD_RIGHT_LOCK_EVT), strlen(GUARD_RIGHT_LOCK_EVT) == 0) return 9;
-    if (strcmp(event, RIGHT_SCAN_EVT), strlen(RIGHT_SCAN_EVT) == 0) return 10;
-    if (strcmp(event, EXIT_EVT), strlen(EXIT_EVT) == 0) return 11;
-    if (strcmp(event, LOCK_DOWN_EVT), strlen(LOCK_DOWN_EVT) == 0) return 12;
-    return -1;  // Unknown event
+//// Function to map user input to an event code
+//int getEventCode(const char *event) {
+//    if (strncmp(event, LEFT_SCAN_EVT, strlen(LEFT_SCAN_EVT)) == 0) return 0;
+//    if (strcmp(event, GUARD_LEFT_UNLOCK_EVT), strlen(GUARD_LEFT_UNLOCK_EVT) == 0) return 1;
+//    if (strcmp(event, LEFT_OPEN_EVT), strlen(LEFT_OPEN_EVT) == 0) return 2;
+//    if (strcmp(event, WEIGHT_EVT), strlen(WEIGHT_EVT) == 0) return 3;
+//    if (strcmp(event, LEFT_CLOSE_EVT), strlen(LEFT_CLOSE_EVT) == 0) return 4;
+//    if (strcmp(event, GUARD_LEFT_LOCK_EVT), strlen(GUARD_LEFT_LOCK_EVT) == 0) return 5;
+//    if (strcmp(event, GUARD_RIGHT_UNLOCK_EVT), strlen(GUARD_RIGHT_UNLOCK_EVT) == 0) return 6;
+//    if (strcmp(event, RIGHT_OPEN_EVT), strlen(RIGHT_OPEN_EVT) == 0) return 7;
+//    if (strcmp(event, RIGHT_CLOSE_EVT), strlen(RIGHT_CLOSE_EVT) == 0) return 8;
+//    if (strcmp(event, GUARD_RIGHT_LOCK_EVT), strlen(GUARD_RIGHT_LOCK_EVT) == 0) return 9;
+//    if (strcmp(event, RIGHT_SCAN_EVT), strlen(RIGHT_SCAN_EVT) == 0) return 10;
+//    if (strcmp(event, EXIT_EVT), strlen(EXIT_EVT) == 0) return 11;
+//    if (strcmp(event, LOCK_DOWN_EVT), strlen(LOCK_DOWN_EVT) == 0) return 12;
+//    return -1;  // Unknown event
 }
 
+
 // Function to handle each event based on the event code
-void handle_event(Person *p, int event_code) {
-    switch (event_code) {
-        case 0:
-            p->state = LEFT_DOOR_SCAN_STATE;
-            get_person_id(p);
-            break;
-        case 1:
-            p->state = GUARD_LEFT_UNLOCK_STATE;
-            break;
-        case 2:
-            p->state = LEFT_DOOR_OPEN_STATE;
-            break;
-        case 3:
-            p->state = WEIGHT_CHECK_STATE;
-            get_weight(p);
-            break;
-        case 4:
-            p->state = LEFT_DOOR_CLOSE_STATE;
-            break;
-        case 5:
-            p->state = GUARD_LEFT_LOCK_STATE;
-            break;
-        case 6:
-            p->state = GUARD_RIGHT_UNLOCK_STATE;
-            break;
-        case 7:
-            p->state = RIGHT_DOOR_OPEN_STATE;
-            break;
-        case 8:
-            p->state = RIGHT_DOOR_CLOSE_STATE;
-            break;
-        case 9:
-            p->state = GUARD_RIGHT_LOCK_STATE;
-            break;
-        case 10:
-            p->state = RIGHT_DOOR_SCAN_STATE;
-            get_person_id(p);
-            break;
-        case 11:
-            p->state = SYSTEM_EXIT_STATE;
-            break;
-        case 12:
-			p->state = LOCK_DOWN_EVT;
-			break;
-        default:
-            printf("Unknown event\n");
-    }
-}
+//void handle_event(Person *p, int event_code) {
+//    switch (event_code) {
+//        case 0:
+//            p->state = LEFT_DOOR_SCAN_STATE;
+//            get_person_id(p);
+//            break;
+//        case 1:
+//            p->state = GUARD_LEFT_UNLOCK_STATE;
+//            break;
+//        case 2:
+//            p->state = LEFT_DOOR_OPEN_STATE;
+//            break;
+//        case 3:
+//            p->state = WEIGHT_CHECK_STATE;
+//            get_weight(p);
+//            break;
+//        case 4:
+//            p->state = LEFT_DOOR_CLOSE_STATE;
+//            break;
+//        case 5:
+//            p->state = GUARD_LEFT_LOCK_STATE;
+//            break;
+//        case 6:
+//            p->state = GUARD_RIGHT_UNLOCK_STATE;
+//            break;
+//        case 7:
+//            p->state = RIGHT_DOOR_OPEN_STATE;
+//            break;
+//        case 8:
+//            p->state = RIGHT_DOOR_CLOSE_STATE;
+//            break;
+//        case 9:
+//            p->state = GUARD_RIGHT_LOCK_STATE;
+//            break;
+//        case 10:
+//            p->state = RIGHT_DOOR_SCAN_STATE;
+//            get_person_id(p);
+//            break;
+//        case 11:
+//            p->state = SYSTEM_EXIT_STATE;
+//            break;
+//        case 12:
+//			p->state = LOCK_DOWN_EVT;
+//			break;
+//        default:
+//            printf("Unknown event\n");
+//    }
+//}
 
 int main(int argc, char *argv[]) {
     Person p;
@@ -134,8 +135,8 @@ int main(int argc, char *argv[]) {
         getUserInput(usrInput);
 
         // Convert user input to an event code and handle the event
-        int event_code = getEventCode(usrInput);
-        handle_event(&p, event_code);
+//        int event_code = getEventCode(usrInput);
+//        handle_event(&p, event_code);
 
         // Send the updated Person struct to the controller
         if (MsgSend(coid, &p, sizeof(p), &ctr, sizeof(ctr)) == -1) {
